fe24b42aaed4c4d5504ce848f19ff451ff011657
pienski <marcin.p.pienkowski@gmail.com>
2026-08-30 18:54:26 +0200

    Moving the login fix to proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -4,6 +4,13 @@ export default withAuth({
   pages: {
     signIn: "/login",
   },
+  callbacks: {
+    authorized: ({ token }) => {
+      // Ensure the user has a valid token with a familyId
+      // This forces old sessions to be logged out and redirects to /login
+      return !!token && !!token.familyId;
+    },
+  },
 });
 
 export const config = {
